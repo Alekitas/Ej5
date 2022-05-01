ae3f02628a5fa0a8d7f4d258ce887d210e8ae90c
Alejo Atienza <73915312+Alekitas@users.noreply.github.com>
2022-05-01 16:08:54 -0300

    Update ManejadorPlanes.py

--- Ej5/ManejadorPlanes.py
+++ Ej5/ManejadorPlanes.py
@@ -19,4 +19,19 @@ class ManejadorPlanes:
             act=input('Ingrese nuevo valor del vehiculo: ')
             self.__lista[i].guardarvar(act)
     def ImporteCuota(self):
-        pass
+        valor=float(input('Ingrese un valor: '))
+        for i in range(len(self.__lista)):
+            valorcuota=(self.__lista[i].getvalorvehiculo/self.__lista[i].getCantCuotas)+self.__lista[i].getvalorvehiculo* 0.10
+            if valor>valorcuota:
+                print('\nCodigo del plan: ',self.__lista[i].getCodigo())
+                print('\nModelo: ',self.__lista[i].getModelo())
+                print('\nVersion del vehiculo: ',self.__lista[i].getVersion())
+    def MostrarMonto(self,valorcuota):
+        for i in range(len(self.__lista)):
+            monto=self.__lista[i].getCantCuotaslic*valorcuota
+            print('Monto que se debe pagar para licitar el vehiculo: ',monto)
+    def ModificarCuotas(self):
+        cod=int(input('\nIngrese un codigo de plan: '))
+        for i in range(len(self.__lista)):
+            mod=input('Ingrese cantidad de cuotas a modificar: ')
+            self.__lista[cod-1].getCantCuotaslic=mod
